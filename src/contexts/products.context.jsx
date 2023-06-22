eb2fdbb1b74be4433924a77aacc7718da570d997
MarcosCabrini Riani_dos_Reis <marcosriani@hotmail.com>
2023-06-22 23:59:37 +0100

    Adding cart context and shopping icon as well as dropdown

---
+++ src/contexts/products.context.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState, useEffect } from 'react';
+
+import PRODUCTS from '../shop-data';
+
+export const ProductsContext = createContext({
+  products: [],
+});
+
+export const ProductProvider = ({ children }) => {
+  const [products, setProduct] = useState(PRODUCTS);
+
+  const value = { products };
+
+  return (
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
